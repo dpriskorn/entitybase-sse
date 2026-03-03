b1454ee82e3502a5f0eae444ad46407cfe0e3696
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-03-03 12:49:22 +0100

    Fix test/error.js to use new errors.js module

--- test/error.js
+++ test/error.js
@@ -6,47 +6,18 @@
 
 const assert = require('assert');
 
-const errors                 = require('../lib/error.js');
-const ExtendableError        = errors.ExtendableError;
+const errors                 = require('../lib/errors');
 const KafkaSSEError           = errors.KafkaSSEError;
-
-
-describe('ExtendableError', () => {
-    it('should construct using string', function() {
-        let m = 'error string';
-        let ee = new ExtendableError(m);
-
-        assert.equal(ee.message, m);
-        assert.equal(ee.name, ExtendableError.name);
-    });
-
-    it('should construct using Error', function() {
-        let m = 'error string';
-        let e = new Error(m)
-        let ee = new ExtendableError(e);
-
-        assert.equal(ee.message, e.message);
-        assert.equal(ee.name, ExtendableError.name);
-        assert.equal(ee.originalName, e.constructor.name);
-    });
-
-    it('should construct using Error with properties', function() {
-        let m = 'error string';
-        let e = new Error(m)
-        e.prop1 = 'a property value';
-        let ee = new ExtendableError(e);
-
-        assert.equal(ee.prop1, e.prop1, 'enumerable properties should be present');
-    });
-});
+const ConfigurationError       = errors.ConfigurationError;
 
 
 describe('KafkaSSEError', () => {
-    it('should construct and set origin', function() {
+    it('should construct and set name', function() {
         let m = 'error string';
         let ke = new KafkaSSEError(m);
 
-        assert.equal(ke.origin, 'KafkaSSE', 'KafkaSSEError.origin should be set to KafkaSSE');
+        assert.equal(ke.message, m);
+        assert.equal(ke.name, 'KafkaSSEError');
     });
 
     it('should construct with extra properties', function() {
@@ -66,5 +37,13 @@ describe('KafkaSSEError', () => {
         assert.equal(deserializedError.message, ke.message);
         assert.equal(deserializedError.name, ke.name);
         assert.equal(deserializedError.prop1, ke.prop1);
-    })
+    });
+});
+
+
+describe('ConfigurationError', () => {
+    it('should be instanceof KafkaSSEError', function() {
+        let ce = new ConfigurationError('test error');
+        assert(ce instanceof KafkaSSEError);
+    });
 });
