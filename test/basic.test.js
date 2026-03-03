7d2dc8f80d44189eca68b6b7e690378a8d3a6816
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-03-03 14:28:02 +0100

    Debug: Add basic test and --no-cache to Jest

---
+++ test/basic.test.js
@@ -0,0 +1,9 @@
+describe('Basic Jest Test', () => {
+    test('should pass', () => {
+        expect(1 + 1).toBe(2);
+    });
+
+    test('should find test files', () => {
+        expect(true).toBe(true);
+    });
+});
